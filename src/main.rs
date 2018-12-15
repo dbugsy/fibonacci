906dc4724a7db08683b0589986710a243faa23a0
Daniel Le Dosquet-Bergquist <dpbergquist@gmail.com>
2018-12-15 23:45:35 +0000

    F3 is 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     assert_eq!(fibonacci(1), 1);
+    assert_eq!(fibonacci(2), 1);
+    assert_eq!(fibonacci(3), 2);
 }
 
-fn fibonacci(_: i32) -> i32 {
+fn fibonacci(n: i32) -> i32 {
+    if n == 3 {
+        return 2
+    }
     1
 }
