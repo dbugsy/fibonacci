152985da651d714f15c7a6dce36569c2aaa1cba1
Daniel Le Dosquet-Bergquist <dpbergquist@gmail.com>
2018-12-16 00:20:26 +0000

    F4 is 3

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ fn main() {
     assert_eq!(fibonacci(1), 1);
     assert_eq!(fibonacci(2), 1);
     assert_eq!(fibonacci(3), 2);
+    assert_eq!(fibonacci(4), 3);
 }
 
 fn fibonacci(n: i32) -> i32 {
-    if n == 3 {
-        return 2
+    if n <= 2 {
+        return 1
     }
-    1
+    fibonacci(n-1) + fibonacci(n-2)
 }
+
